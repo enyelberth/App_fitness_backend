1d6c703854f4d07d44d9d7ec1778471ae483f6f4
Enyelberth Rodriguez <64881597+enyelberth@users.noreply.github.com>
2024-09-13 14:23:09 -0400

    Update userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -16,26 +16,35 @@ export const GetUsers = async (req: Request, res: Response) => {
   });
 };
 export const GetUser = async (req: Request, res: Response) => {
+  
+  // Recojemos el parametro que mandamos por la url por http ese es el parametro que le mandamos  
   const { cliente } = req.params;
+  //codigo que se lo pasamos al sevicio para buscar al user
   const { data, status, message } = await getUser(parseInt(cliente));
-
+  //Retornamos los datos un mensaje y un status 
   return res.status(status).json({
     message,
     data,
   });
+  
 };
-
 export const CreateNewUser = async (req: Request, res: Response) => {
   const dato = req.body;
+  
+  
   // errror resolver despues
   console.log(dato);
   const { status, message } = await createNewUser(dato);
-
+  //No me recuedo el error creo que es validator paramtros del usuarios 
   return res.status(status).json({
     message,
   });
+
+  
 };
 export const updateUser = (req: Request, res: Response) => {
+ const { cliente } = req.params;
+  //terminar el update 
   res.send("Update  user");
 };
 export const DeleteUser = async (req: Request, res: Response) => {
